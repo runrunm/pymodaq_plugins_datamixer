9d137fc2b792d0da56d2c1604ed5f34bdc78e092
Sébastien Weber <seba.weber@gmail.com>
2024-10-16 13:57:29 +0200

    version patch

--- src/pymodaq_plugins_datamixer/__init__.py
+++ src/pymodaq_plugins_datamixer/__init__.py
@@ -1,8 +1,13 @@
 from pathlib import Path
 from pymodaq_utils.logger import set_logger  # to be imported by other modules.
-from _version import __version__, __version_tuple__, version_tuple, version
+from pymodaq_utils.utils import get_version, PackageNotFoundError
 
 from .utils import Config
+
 config = Config()
+try:
+    __version__ = get_version(__package__)
+except PackageNotFoundError:
+    __version__ = '0.0.0dev'
 
 
